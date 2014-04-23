95ff6eb5242909ac440584cd30e771d8c22a684a
Jim Richardson <jim.richardson.dcs@gmail.com>
2014-04-23 10:32:19 -0400

    still playing with branches, fresh commit

--- CppMisc/Main.cpp
+++ CppMisc/Main.cpp
@@ -6,7 +6,6 @@
 
 int main()
 {
-	std::cout << "message added on master branch." << std::endl;
 	std::cout << "Swapping:" << std::endl;
 	int i = 2, j = 3;
     std::cout << "i=" << i << ", j=" << j << std::endl;
@@ -24,8 +23,6 @@ int main()
 	std::cout << "c1=" << c1 << ", c2=" << c2 << std::endl;
 	GenDemo::swap(c1, c2);
 	std::cout << "c1=" << c1 << ", c2=" << c2 << std::endl;
-
-	std::cout << "Message added on test branch." << std::endl;
 
 	system("pause");
     return 0;
